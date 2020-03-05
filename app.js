91dc9146ec7f17dbbe1f57f076c0a77f815dc61d
Millie Phimmasone <59859457+mphimm@users.noreply.github.com>
2020-03-05 13:33:37 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import FormData from './Components/FormData';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Register Form:</h1>
+      <FormData/>
+    </div>
+  );
+}
+
+export default App;
